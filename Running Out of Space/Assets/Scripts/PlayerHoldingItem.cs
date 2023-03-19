ff3e528b6b6ef9bcd9105eb2677792ff8b3633c2
Atsu35 <atsushirakaki4649@gmail.com>
2023-03-18 20:18:19 -0700

    Made character hold and drop items

---
+++ Running Out of Space/Assets/Scripts/PlayerHoldingItem.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHoldingItem : MonoBehaviour
+{
+    [SerializeField] private Transform playerCameraTransform;
+    [SerializeField] private Transform objectGrabPointTransform;
+    [SerializeField] private LayerMask pickUpLayerMask;
+
+    private Grabbable objectGrabbable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (objectGrabbable == null)
+            {
+                // Not carring an object, try to grab
+                float pickUpDistance = 2f;
+                if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out RaycastHit raycastHit, pickUpDistance, pickUpLayerMask))
+                {
+                    //Debug.Log(Raycast.transform);
+                    if (raycastHit.transform.TryGetComponent(out objectGrabbable))
+                    {
+                        objectGrabbable.Grab(objectGrabPointTransform);
+                    }
+                }
+            }
+            else
+            {
+                //Currently carrying an object, drop
+                objectGrabbable.Drop();
+                objectGrabbable = null;
+            }
+        }
+    }
+}
